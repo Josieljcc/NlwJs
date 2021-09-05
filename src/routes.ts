d121ce613884da48d206edefeaebd2f38851ea19
Josiel da Costa Carvalho <josiel.jcc@hotmail.com>
2021-09-05 13:42:34 -0300

    Tag Althorization

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,15 @@
 import {Router} from 'express'
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserCotroller } from './controllers/CreateUserController'
+import { ensureAdmin } from './middlewares/ensureAdmin'
 
 
 const router = Router()
+
 const createUserCotroller = new CreateUserCotroller()
 const createTagController = new CreateTagController()
 router.post("/users", createUserCotroller.handle)
-router.post("/tags", createTagController.handle)
+router.post("/tags", ensureAdmin, createTagController.handle)
 router.get("/test", (req,res) => console.log("funcionou"))
 
 
