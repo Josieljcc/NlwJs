e7099ac62f46e7db5cfd5ebff62e1e7927d4a91c
Josiel da Costa Carvalho <josiel.jcc@hotmail.com>
2021-09-06 21:34:11 -0300

    Authenticate users

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateComplimentController } from './controllers/CreateComplimentContro
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserCotroller } from './controllers/CreateUserController'
 import { ensureAdmin } from './middlewares/ensureAdmin'
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated'
 
 
 const router = Router()
@@ -14,9 +15,9 @@ const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
 
 router.post("/users", createUserCotroller.handle)
-router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
-router.post("/compliments", createComplimentController.handle)
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle)
 router.get("/test", (req,res) => console.log("funcionou"))
 
 
